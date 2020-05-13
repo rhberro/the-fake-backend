873033e49f17220d8bc3e0359eb3e05e57f30236
Fernando Maia <fernando.maia@ifood.com.br>
2020-05-12 21:49:14 -0300

    feat: create `Request` and `Response` interfaces

---
+++ source/interfaces/Request.ts
@@ -0,0 +1,3 @@
+import express from 'express';
+
+export default interface Request extends express.Request {}
---
+++ source/interfaces/Response.ts
@@ -0,0 +1,3 @@
+import express from 'express';
+
+export default interface Response extends express.Response {}
